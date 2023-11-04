0d3b40cc13ed9387554ea5c22bd15acc96167b53
ophirbucai <oaywan@icloud.com>
2023-11-04 19:45:07 +0200

    Fix: Add return type to useMyPlaylist hook

---
+++ src/hooks/useMyPlaylist.tsx
@@ -0,0 +1,6 @@
+import { useContext } from "react";
+import { MyPlaylistContext } from "../context/myPlaylist/myPlaylist.context";
+import type { TMyPlaylistContext } from "../context/myPlaylist/myPlaylist.context.type";
+
+const useMyPlaylist = (): TMyPlaylistContext => useContext(MyPlaylistContext);
+export default useMyPlaylist;
