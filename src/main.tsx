da5011de78026bc270802bdddc53a07810e35c3c
ophirbucai <oaywan@icloud.com>
2023-11-05 19:20:17 +0200

    Fix: Replace BrowserRouter with HashRouter to support GH Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from "react-router-dom";
 import AppRouter from './AppRouter.tsx';
 import AppLayout from "./AppLayout.tsx";
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
-import { BrowserRouter } from "react-router-dom";
 import CssBaseline from "@mui/material/CssBaseline";
 import { theme } from "./lib/constants";
 import './index.css';
@@ -11,11 +11,11 @@ import './index.css';
 ReactDOM.createRoot(document.body!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename="/react-portfolio">
+      <HashRouter>
         <AppLayout>
           <AppRouter/>
         </AppLayout>
-      </BrowserRouter>
+      </HashRouter>
       <CssBaseline />
     </ThemeProvider>
   </React.StrictMode>,
