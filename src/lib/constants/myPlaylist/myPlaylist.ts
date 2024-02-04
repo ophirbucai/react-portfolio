ac31ac008ecd9e38bae96743ee5801b4f74c0464
Ophir Bucai <ophirbucai@gmail.com>
2024-02-05 00:14:02 +0200

    Resolve PR comments

--- src/lib/constants/myPlaylist/myPlaylist.ts
+++ src/lib/constants/myPlaylist/myPlaylist.ts
@@ -1,5 +1,6 @@
 export const myPlaylist = [
     { title: 'We Shall Overcome (Live in London 1965) 🫶🏾🎶', artist: 'Joan Baez', album: 'Live In London 1965', year: 2016, duration: 3 * 60 + 58, path: 'LiDTvcRRZa0' },
-    // { title: 'The Night They Drove Old Dixie Down (Live in London 1965)', artist: 'Joan Baez', album: 'Live In London 1965', year: 2016, duration: 3 * 60 + 21, path: '', ratio: 16 / 9 },
-    // { title: 'This Land Is Your Land', artist: 'Bruce Springsteen', album: '', path: 'nZBmhvVYJxs', ratio: 16 / 9 } // 1yuc4BI5NWU
+    /* TODO: Add more songs/playlists to play, ensure they are available when app starts up
+    { title: 'The Night They Drove Old Dixie Down (Live in London 1965)', artist: 'Joan Baez', album: 'Live In London 1965', year: 2016, duration: 3 * 60 + 21, path: '', ratio: 16 / 9 },
+    { title: 'This Land Is Your Land', artist: 'Bruce Springsteen', album: '', path: 'nZBmhvVYJxs', ratio: 16 / 9 } // 1yuc4BI5NWU */
 ]
