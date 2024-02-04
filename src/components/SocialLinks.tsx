00ccc105e1fd1fb3756de70eccd402b0aac974d0
Ophir Bucai <ophirbucai@gmail.com>
2024-02-04 23:55:59 +0200

    Add social links to layout

---
+++ src/components/SocialLinks.tsx
@@ -0,0 +1,18 @@
+import GithubIcon from '../assets/icons/github.svg'
+import LinkedinIcon from '../assets/icons/linkedin.svg'
+import GitIcon from '../assets/icons/git.svg'
+export const SocialLinks = () => {
+    return (
+        <>
+            <li>
+                <a href='https://github.com/ophirbucai'><GithubIcon/></a>
+            </li>
+            <li>
+                <a href='https://linkedin.com/in/ophirbucai'><LinkedinIcon/></a>
+            </li>
+            <li>
+                <a href='https://github.com/ophirbucai/react-portfolio'><GitIcon /></a>
+            </li>
+        </>
+    )
+}
