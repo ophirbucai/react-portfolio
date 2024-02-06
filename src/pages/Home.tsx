ebc94fc2853be90d7ee48097bb369f339cc27690
Ophir Bucai <ophirbucai@gmail.com>
2024-02-06 18:37:26 +0200

    Add landing page content and style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,32 @@
+import { Button } from '@mui/material'
+import useTypewriter from '../hooks/useTypewriter.tsx'
+
 const Home = () => {
+    const { word } = useTypewriter(['digital web experiences ', 'engaging websites'], 75)
     return (
         <div className='landing'>
-            <header className='landing__title'>
-                <h1>Ophir Bucai</h1>
-            </header>
-            <div>
-                <p>Hey there! Good to see you</p>
-            </div>
+            <hgroup className='landing__title'>
+                <h2>Building</h2>
+                <h1>{word}</h1>
+            </hgroup>
+            <section>
+                <h3 className='landing__subtitle'>Who is this website
+                    for?</h3>{/*<p>Making websites pop out and look amazing takes careful consideration, it can only be built bit by bit.</p>*/}
+                <p>No good job gets done if you don't learn from what you did before that worked, and what you did that
+                    didn't work.</p>
+                <p>As it turns out, I like to develop websites that not only look amazing, but be as flawless as can be,
+                    on every device.</p>
+                <Button className='landing__button' variant='contained' size='small'>my project</Button>
+            </section>
+            <section>
+                <h3 className='landing__subtitle'>Scroll to learn more about
+                    me</h3>{/*<p>Making websites pop out and look amazing takes careful consideration, it can only be built bit by bit.</p>*/}
+                <p>Making a vertical website isn't an easy task. But I took the challenging route, it's not always the
+                    best option.</p>
+                <p>Sometimes you got to take the blue pill -- but eventually, you will want to work through the caveats
+                    to be a real pro.</p>
+                <p>It takes patience, persistence and burning the midnight oil becomes a habit.</p>
+            </section>
         </div>
     )
 }
