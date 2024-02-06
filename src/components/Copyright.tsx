99e31f3033416d73ca7dee9fa2e9c89e843a353d
Ophir Bucai <ophirbucai@gmail.com>
2024-02-06 19:38:47 +0200

    Make copyright text color visible

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -2,7 +2,7 @@ import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
 
 export const Copyright = () => (
-  <Typography variant='body2' color='text.secondary' align='center'>
+  <Typography variant='body2' align='center'>
     {'Copyright © '}
     <Link color='inherit' href='https://github.com/ophirbucai'>
       Ophir Bucai
